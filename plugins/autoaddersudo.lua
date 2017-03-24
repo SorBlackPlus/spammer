8ead5b34de0b0df0e21a41eb288bac397ee29159
SorBlack <sorblackplus@gmail.com>
2017-03-24 10:57:30 -0700

    updated :)

--- plugins/autoaddersudo.lua
+++ plugins/autoaddersudo.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
 local bot_id = our_id 
-local fbot1 = 255317894 --idi khodetoono inja bezanid
+local fbot1 = 93365812 --idi khodetoono inja bezanid
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
     end
 end
